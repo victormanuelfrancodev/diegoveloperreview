bc38222d2a341458099debcbfac89af418fd612f
Victor Manuel Lagunas Franco <victormanuelfrancodev@gmail.com>
2023-09-18 10:21:00 +0900

    add ux ui in the list products

--- lib/widgets/cell_list_products.dart
+++ lib/widgets/cell_list_products.dart
@@ -9,32 +9,59 @@ class CellListProducts extends StatelessWidget {
   Resultproduct product;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => DetailProduct(product: product)));
-      },
-      child: ListTile(
-        title: Text(product.title,
+    return Card(
+      elevation: 5.0,
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => DetailProduct(product: product)));
+        },
+        child: ListTile(
+          contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+          title: Text(
+            product.title,
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 23,
-            )),
-        subtitle: Text(product.description,
-            style: TextStyle(
-              fontSize: 18,
-            )),
-        leading: Hero(
-          tag: "Hero-${product.id}",
-          child: CachedNetworkImage(
-            imageUrl: product.images[0],
-            placeholder: (context, url) => CircularProgressIndicator(),
-            errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: Text(
+              product.description,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
+          leading: Hero(
+            tag: "Hero-${product.id}",
+            child: Container(
+              width: 60.0,
+              height: 60.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.blue, width: 2.0),
+              ),
+              child: ClipOval(
+                child: CachedNetworkImage(
+                  imageUrl: product.images[0],
+                  placeholder: (context, url) => CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
           ),
         ),
       ),
     );
+
   }
 }
