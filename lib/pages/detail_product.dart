2c0195ac1fa7f3313aa2f257c12cfc97a20f35b1
Victor Manuel Lagunas Franco <victormanuelfrancodev@gmail.com>
2023-09-18 10:22:32 +0900

    add ux detail product

--- lib/pages/detail_product.dart
+++ lib/pages/detail_product.dart
@@ -14,71 +14,102 @@ class DetailProduct extends StatelessWidget {
       appBar: AppBar(
         title: Text(product.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Text(
-            product.title,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 23,
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Text(product.description),
-          Text("Category:"),
-          Text(product.category.name.toString()),
-          Container(
-            width: 25,
-            height: 25,
-            child: CachedNetworkImage(
-              imageUrl: product.category.image,
-              placeholder: (context, url) => Center(
-                  child: Container(
-                      width: 50,
-                      height: 50,
-                      child: CircularProgressIndicator())),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-            ),
-          ),
-          CarouselSlider(
-            options: CarouselOptions(height: 400.0),
-            items: product.images.asMap().entries.map((entry) {
-              int idMapImage = entry.key;
-              String image = entry.value;
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      child: idMapImage == 0
-                          ? Hero(
-                        tag: 'Hero-${product.id}',
-                        child: CachedNetworkImage(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                product.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26,
+                ),
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                product.description,
+                style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                "Category:",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Row(
+                children: [
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      border: Border.all(color: Colors.blue, width: 1.5),
+                    ),
+                    child: ClipOval(
+                      child: CachedNetworkImage(
+                        imageUrl: product.category.image,
+                        placeholder: (context, url) => Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10.0),
+                  Expanded(
+                    child: Text(
+                      product.category.name.toString(),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              CarouselSlider(
+                options: CarouselOptions(height: 400.0),
+                items: product.images.asMap().entries.map((entry) {
+                  int idMapImage = entry.key;
+                  String image = entry.value;
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        child: idMapImage == 0
+                            ? Hero(
+                          tag: 'Hero-${product.id}',
+                          child: CachedNetworkImage(
+                            imageUrl: image,
+                            placeholder: (context, url) => Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
+                        )
+                            : CachedNetworkImage(
                           imageUrl: image,
                           placeholder: (context, url) => Center(
-                              child: Container(
-                                  width: 50,
-                                  height: 50,
-                                  child: CircularProgressIndicator())),
+                            child: CircularProgressIndicator(),
+                          ),
                           errorWidget: (context, url, error) =>
                               Icon(Icons.error),
                         ),
-                      )
-                          : CachedNetworkImage(
-                        imageUrl: image,
-                        placeholder: (context, url) => Center(
-                            child: Container(
-                                width: 50,
-                                height: 50,
-                                child: CircularProgressIndicator())),
-                        errorWidget: (context, url, error) =>
-                            Icon(Icons.error),
-                      ));
-                },
-              );
-            }).toList(),
-          )
-        ],
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 20.0),
+            ],
+          ),
+        ),
       ),
     );
   }
